b65fea012ddd679a643f179c86bcc2c3d8ce8fcf
Edwin Okoronkwo <edwinokoronkwo@gmail.com>
2025-03-27 18:44:20 -0600

    created application-test.properties

--- src/test/java/com/edwinokoronkwo/hotelstaffservice/HotelStaffServiceApplicationTests.java
+++ src/test/java/com/edwinokoronkwo/hotelstaffservice/HotelStaffServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.edwinokoronkwo.hotelstaffservice;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 
 @SpringBootTest
+@EnabledIf(expression = "#{environment.acceptsProfiles('mysql')}", reason = "Disabled in GitHub Actions")
 class HotelStaffServiceApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
